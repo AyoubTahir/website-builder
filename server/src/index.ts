36a858a48558636491272ebfdda23d56b70ace03
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-08-14 21:08:19 +0100

    Adding nodemon and docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//test api
+app.get("/users", (req, res) => {
+  try {
+    const users = prisma.user.findMany();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+//start server
+const PORT = /*process.env.PORT ||*/ 4001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
